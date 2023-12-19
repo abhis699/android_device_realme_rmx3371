cf62888e91523da9a0038e152fec8a14c0a0314e
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-12-19 13:57:06 +0530

    RMX3371: Update required nodes for OplusExtras

--- device.mk
+++ device.mk
@@ -278,6 +278,7 @@ PRODUCT_PACKAGES += \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
     init.oplus.sh \
+    init.oplus_extras.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.spartan.rc \
